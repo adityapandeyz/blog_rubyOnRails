90bdd06bc30a39582ef1de651b414774827d9c19
Aditya Pandey <adityapandeyz@outlook.com>
2024-05-12 22:06:06 +0530

    2nd Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :blog_posts
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_pot
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+user = User.where(email: "test@gmail").first_or_initialize
+user.update!(
+  password: "password",
+  # passsword_confirmation: "password"
+)
